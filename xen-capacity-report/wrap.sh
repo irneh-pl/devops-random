937ebb9ec941d0d4a08bdfcfc5cc7cf49e048f58
Henri Gabriel <henri@pulselocker.com>
2015-11-09 13:28:22 -0800

    Add aggregation.

--- xen-capacity-report/wrap.sh
+++ xen-capacity-report/wrap.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-echo "Host     VMs UsedCPU FreeCPU UsedRAM FreeRAM UsedDisk FreeDisk" && ./summarize.awk extract.csv | sort
+echo "Host     VMs UsedCPU FreeCPU UsedRAM FreeRAM UsedDisk FreeDisk" \
+  && ./summarize.awk extract.csv \
+  | awk -F" " '
+    NR>1 {
+      CPU+=$3;
+      TotCPU+=$3+$4;
+      RAM+=$5;
+      TotRAM+=$5+$6;
+      Disk+=$7;
+      TotDisk+=$7+$8;
+      print
+    }
+    END {
+      printf "%19.0f%% %14.0f%% %15.0f%%\n",
+      CPU/TotCPU*100,
+      RAM/TotRAM*100,
+      Disk/TotDisk*100
+    }' \
+  | sort -g
